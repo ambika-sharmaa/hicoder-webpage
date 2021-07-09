77a46de4bea0e2a1ae3e83d8f793cef329ba1250
Ambika Sharma <60319658+ambika-sharmaa@users.noreply.github.com>
2021-07-10 01:59:18 +0530

    Create Data.js

---
+++ src/components/pages/Home page/Data.js
@@ -0,0 +1,87 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'YOUR TIME, OUR EFFORTS',
+    headline: 'Pay After Job Full Stack Development Course',
+    buttonLabel: 'KNOW MORE',
+    imgStart: '',
+    img: 'images/man-working.jpg',
+    alt: 'man working'
+  };
+
+  export const secObjOne={
+      lightBg:false,
+      topLine:'NEXT BATCH: 27 July | Apply Now'
+  };
+  
+  export const homeObjTwo = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'HICODER',
+    headline: 'We help students learn Full Stack Development.',
+    description:
+      "HiCoder does everything that helps students become successful in the software industry. From product thinking to product designing, system designing, choosing the right frameworks, breaking projects in stages to building teams, and working collaboratively to making the application comply with the latest Google SEO needs and knowledge about integrated analytics, we do everything in between.",
+    buttonLabel: 'About Us',
+    imgStart: 'start',
+    img: 'images/lady.jpg',
+    alt: 'Vault'
+  };
+  
+  export const homeObjThree = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: 'Trained on the most in-demand Technology Skills',
+    description:
+      "At the end of the course, every student becomes capable to develop their own create a web application, host on their own domain, enable advanced technologies and observe people's actions on their website and represent themselves as a HiCoder that matters.",
+    buttonLabel: 'Apply Now',
+    imgStart: '',
+    img: 'images/laptops3.jpg',
+    alt: 'Vault'
+  };
+
+  export const secObjTwo={
+    lightBg:false,
+    topLine:'34+ Projects completed - 47+ Industry experts'
+};
+
+export const thirdObjOne={
+    Line1:'34+ PROJECTS COMPLETED',
+    Line2: '47+ INDUSTRY EXPERTS',
+    Line3: '30+ QUALITY DEVELOPERS',
+    Line4: '40,000,00+ APPLICATION USERS'
+};
+  
+  export const homeObjFour = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: 'How does HiCoder help?',
+    description:
+      'HiCoder understands the struggle, peer pressure an engineering student goes into throughout curriculum. We believe, Most engineering students who can be part of the core software industry end up having a tech support or customer care job just because college never taught trending technology, project Management or encouraged students to their own projects.      ',
+    buttonLabel: 'KNOW MORE',
+    imgStart: 'start',
+    img: 'images/discussing.jpg',
+    alt: 'Vault'
+  };
+
+  export const fourthObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: 'Why HiCoder?',
+    description:
+      'HiCoder enables students to think about real-world problems, design, and develop the right solution. ',
+    heading1:'Personalized Training',
+    content1:'We understand the weakness of students coming from poor educational backgrounds. HiCoder avails resources and extra after-class support to match the criteria and keep up with tech.',
+    heading2:'Production Projects',
+    content2:'Every student ends up making around 3 projects that are wholesome, cloud-hosted and can be used for a commercial purpose.',
+    imgStart: '',
+    img: 'images/teaching.jpg',
+    alt: 'Vault'
+  };
